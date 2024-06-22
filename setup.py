b81b6fce394369532ea94de1a05a0d62807131c1
AkbarRahmat <146505525+AkbarRahmat@users.noreply.github.com>
2024-06-22 22:17:59 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fp_Pencarian_Pestisida',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'Flask>=2.0.0',  
+        'pandas>=1.0.0' 
+    ],
+)
