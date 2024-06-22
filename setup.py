cd5f4e99d439cec59da37b8f2686693632a99c2e
AkbarRahmat <146505525+AkbarRahmat@users.noreply.github.com>
2024-06-22 22:29:20 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'Flask>=2.0.0',  
-        'pandas>=1.0.0' 
+        'Flask>=2.0.0',
+        'pandas>=1.0.0'
     ],
 )
